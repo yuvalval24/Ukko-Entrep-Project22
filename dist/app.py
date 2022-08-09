0c3815da63f2cf2663ea030aadb3351c9aa5183c
yuvalv23meet <92042419+yuvalval24@users.noreply.github.com>
2022-08-09 16:53:14 +0300

    established database

--- dist/app.py
+++ dist/app.py
@@ -5,15 +5,15 @@ import pyrebase
 
 
 config = {
-  "apiKey": "AIzaSyD1rctb8fdxnTAq_MM4m010BaZKk4NR4Go",
-  "authDomain": "personal-project-seddit-y2s.firebaseapp.com",
-  "databaseURL": "https://personal-project-seddit-y2s-default-rtdb.europe-west1.firebasedatabase.app",
-  "projectId": "personal-project-seddit-y2s",
-  "storageBucket": "personal-project-seddit-y2s.appspot.com",
-  "messagingSenderId": "552845203678",
-  "appId": "1:552845203678:web:3e4f353775f5bd3e398a4f",
-  "measurementId": "G-M9L3JDSZ5D",
-  "databaseURL" : "https://personal-project-seddit-y2s-default-rtdb.europe-west1.firebasedatabase.app"
+  "apiKey": "AIzaSyBIY-LB6X66OoVFNi27eZcVPCpreDN7C-Y",
+  "authDomain": "entrep-site-summer-2022.firebaseapp.com",
+  "databaseURL": "https://entrep-site-summer-2022-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "entrep-site-summer-2022",
+  "storageBucket": "entrep-site-summer-2022.appspot.com",
+  "messagingSenderId": "1077886875371",
+  "appId": "1:1077886875371:web:2b56572f5a95868b6730ae",
+  "measurementId": "G-XE5XPTY1JD",
+  "databaseURL" : "https://entrep-site-summer-2022-default-rtdb.europe-west1.firebasedatabase.app"
 }
 
 app = Flask(  # Create a flask app
@@ -31,14 +31,24 @@ db = firebase.database()
 
 @app.route('/', methods = ["get", "post"])
 def home():
-    print('post0')
-    if request.method == "post":
-        print('post1')
+    print(request.method)
+    if request.method == "POST":
         try:
             email = request.form["email"]
-            print("email:", email)
+            password = request.form["password"]
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
         except:
-            print("failed")
+            print("not sign in")
+        try:
+            name = request.form["name"]
+            email = request.form["email"]
+            password = request.form["password"]
+            message = request.form["message"]
+            # print(name+"\n"+email+"\n"+password+"\n"+message)
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            db.child('Users').child(login_session["user"]["localId"]).set({"email":email, "password":password, "name":name, "message":message})
+        except:
+            print("not sign up")
     return render_template("post.html")
 
 @app.route('/Q&A', methods = ["get", "post"])
@@ -51,14 +61,6 @@ def media():
 
 @app.route('/sign', methods = ["get", "post"])
 def sign():
-    print('post0')
-    if request.method == "post":
-        print('post1')
-        try:
-            email = request.form["email"]
-            print("email:", email)
-        except:
-            print("failed")
     return render_template("contact.html")
 
 if __name__ == "__main__":  # Makes sure this is the main process
